6853963316f6deb69dc96bf32c5287b325c9693c
Anita <103582907+AnitaSharma2022@users.noreply.github.com>
2022-10-13 13:22:28 +0200

    added control flow some excercises

--- 2-control-flow/1-isolate/1-conditional-statements/exercises/2-initial-values/3.js
+++ 2-control-flow/1-isolate/1-conditional-statements/exercises/2-initial-values/3.js
@@ -8,18 +8,18 @@ console.log('-- begin --');
 //  what do all solutions to the same path have in common?
 // or maybe there are unreachable paths!
 
-const value1 = _;
-const value2 = _;
+const value1 = false;
+const value2 = false;
 let path = '';
 
 if (value1 && !value2) {
-  path = 'if';
+  path = 'if';   // value1= true and value 2=false
 } else if (!value1 || !value2) {
-  path = 'else if 1';
+  path = 'else if 1';  // value1= false and value 2=true
 } else if (value2 && !value1) {
-  path = 'else if 2';
+  path = 'else if 2';   // unreachable path
 } else {
-  path = 'else';
+  path = 'else';  // unreachable path
 }
 
 console.log(path);
